58ffc508f65a908fbd4f062b1085e55c56e8140c
isaac <isaackayembe44@gmail.com>
2025-04-28 20:45:35 +0200

    Description des nouvelles modifications

--- mariage/settings.py
+++ mariage/settings.py
@@ -14,11 +14,6 @@ from pathlib import Path
 import os
 
 
-CSRF_TRUSTED_ORIGINS = [
-    os.environ.get('APP_URL'),
-]
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -45,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
      'formulaire',
+     'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'corsheaders.middleware.CorsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True  # Accepte toutes les origines
 
 ROOT_URLCONF = 'mariage.urls'
 
